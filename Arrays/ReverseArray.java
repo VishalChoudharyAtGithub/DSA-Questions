75a0c77354a446e6048ff21bc8e3705a8e0a7b35
Vishal Choudhary <47331674+VishalChoudharyGithub@users.noreply.github.com>
2021-01-31 21:46:43 +0530

    Update ReverseArray.java

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,12 +1,13 @@
 public class ReverseArray {
-
-	static void rvereseArray(int arr[], int start , int end) {		
+	static void reverseArray(int arr[], int start , int end) {		
+		//[2,3,6,8,1,5]
+		// Idea is to swap (2,5), then (3,1) and finally (6,8) then stop.
 		while (start < end) {
 			swap(arr, start, end);
 			start++;
 			end--;
 	} 
-    static void swap(int[] arr, int index1,int index2){
+	static void swap(int[] arr, int index1,int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
