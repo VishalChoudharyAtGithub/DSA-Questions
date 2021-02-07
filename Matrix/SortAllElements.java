41e5a02804268294f79c98e0ff0c478dd416af4b
Vishal Choudhary <47331674+VishalChoudharyGithub@users.noreply.github.com>
2021-02-07 22:04:41 +0530

    Create SortAllElements.java

---
+++ Matrix/SortAllElements.java
@@ -0,0 +1,19 @@
+class Solution {
+    int[][] sortedMatrix(int N, int Mat[][]) {
+        // code here
+        int[] arr = new int[N*N];
+        int index = 0;
+        for(int[] subArr: Mat){
+            for(int val : subArr){
+                arr[index++] = val;
+            }
+        }
+        Arrays.sort(arr);
+        index = 0;
+        for(int i = 0;i<N;i++)
+            for(int j = 0;j<N;j++)
+                Mat[i][j] = arr[index++];
+                
+        return Mat;
+    }
+}
