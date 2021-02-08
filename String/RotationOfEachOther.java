1bbb56732932c35ad0e1350b3ae03d815e11db68
Vishal Choudhary <47331674+VishalChoudharyGithub@users.noreply.github.com>
2021-02-08 23:35:47 +0530

    Update RotationOfEachOther.java

--- String/RotationOfEachOther.java
+++ String/RotationOfEachOther.java
@@ -1,3 +1,5 @@
+// Problem link - https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/
+
 class Solution {
     public boolean areRotations(String str1, String str2) 
     {   
@@ -5,4 +7,12 @@ class Solution {
                ((str1 + str1).indexOf(str2) != -1); 
     }    
 }
+
+// There length should be same
+// If we concatenate s1 with itself then s2 must be its substring
+// eg : s1 = hello
+//      s2 = lohel
+// s1 + s2 = hellohello
+//              |||||
+//              lohel  <- this is s2 in s1 
 
