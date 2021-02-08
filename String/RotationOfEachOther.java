9ba3890f2f9044d521101952bcae9c81728ef527
Vishal Choudhary <47331674+VishalChoudharyGithub@users.noreply.github.com>
2021-02-08 23:34:53 +0530

    Create RotationOfEachOther.java

---
+++ String/RotationOfEachOther.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean areRotations(String str1, String str2) 
+    {   
+        return (str1.length() == str2.length()) && 
+               ((str1 + str1).indexOf(str2) != -1); 
+    }    
+}
+
